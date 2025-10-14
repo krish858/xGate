7b46a34531287540ae794385410b9f47215e1e8a
krish pandey <143093900+krish858@users.noreply.github.com>
2025-10-14 08:45:00 +0530

    boilerplate

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router";
+import Main from "./pages/Main";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
